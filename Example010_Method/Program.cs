abf11603a12953c8b2296b88f4f4129c32e27584
shiverskikh <katerina_kononenko_94@mail.ru>
2022-10-15 17:22:08 +0700

    add example 010 Method

---
+++ Example010_Method/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 18, 22, 83, 74, 15, 96, 857, 98, 96 };
+
+int n = array.Length;
+int find = 96;
+
+int index = 0;
+ while (index < n)
+ {
+
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+
+    index = index + 1;
+ }
