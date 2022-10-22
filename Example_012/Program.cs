0c7d422e55f845151cf7b5646c3c2474ec558a5e
shiverskikh <katerina_kononenko_94@mail.ru>
2022-10-22 16:38:17 +0700

    add example 12

---
+++ Example_012/Program.cs
@@ -0,0 +1,49 @@
+﻿//Вид 1:
+/*void Method1()
+{
+    Console.WriteLine("Автор Шиверских Екатерина ");
+}
+Method1();
+
+
+
+//Вид2:
+void Method2(string msd)
+{
+    Console.WriteLine(msd);
+}
+//Method2("Текст сообщения ");*/
+
+/*void Method21(string msg, int count)
+{
+    int i = 0;
+    while(i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+Method21(msg: "Text", count: 4);*/
+//Вид3
+/*int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);*/
+//Вид4
+/*string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+    while(i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "asdf");
+Console.WriteLine(res);*/
+
+
