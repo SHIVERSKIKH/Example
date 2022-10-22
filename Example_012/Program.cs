2238b528964aac82df9532dde0249d73867d5fbe
shiverskikh <katerina_kononenko_94@mail.ru>
2022-10-22 22:26:06 +0700

    add program example 12

--- Example_012/Program.cs
+++ Example_012/Program.cs
@@ -46,4 +46,56 @@ Console.WriteLine(year);*/
 string res = Method4(10, "asdf");
 Console.WriteLine(res);*/
 
+/*string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for(int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(10, "asdf");
+Console.WriteLine(res);*/
+
+/*for(int i = 2; i <= 10; i++)
+{
+    for(int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} * {j} = {i * j} ");
+    }
+    Console.WriteLine();
+}
+*/
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что,"
+            + "ежели бы вас послали вместого нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+//string s = "qwerty"
+//            01234
+// s[3] // r 
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for(int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+string newText = Replace(text, ' ', '|' );
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К' );
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'С', 'с' );
+Console.WriteLine(newText);
+Console.WriteLine();
 
